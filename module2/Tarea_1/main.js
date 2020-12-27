e5cf00a5491c697c4677cc0224b20706672472f8
AlvaroGomezIng <alvaro.gomez.ing@gmail.com>
2020-12-26 21:11:51 -0300

    initial commit

---
+++ module2/Tarea_1/main.js
@@ -0,0 +1,10 @@
+/* Variable declaration */
+var nombre;
+var admin;
+
+/* Value assignation */
+nombre = "John";
+admin = nombre;
+
+/* Writing the variable value in html page */
+document.write(admin);
